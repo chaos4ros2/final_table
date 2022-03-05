7898282ae8585ee3aefb8f12b37a791355547211
chaos4ros2 <liuchao46642@yahoo.co.jp>
2022-03-05 16:47:26 +0900

    fix deploy error [rel=noreferrer]

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -32,7 +32,7 @@ export const Nav = (props: Props) => {
                     alt='Netflix Logo'
                 />
             </a>
-            <a href='https://www.youtube.com/watch?v=m8gpzke_BTE' target='_blank'  rel='noreferrer'>
+            <a href='https://www.youtube.com/watch?v=m8gpzke_BTE' target='_blank' rel='noreferrer'>
                 <img
                     className={styles['Nav-avater']}
                     src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png'
