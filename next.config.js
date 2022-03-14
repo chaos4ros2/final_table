1020239bfa4792c2d98758d4728490657ddf59ae
chaos4ros2 <liuchao46642@yahoo.co.jp>
2022-03-14 18:27:05 +0900

    delete proxy config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  axios: {
-    proxy: true
-  },
-  proxy: {
-    'https://api.chefkoch.de/v2/recipes': {
-      target: 'https://www.chefkoch.de/rezepte/',
-    }
-}
 }
 
 module.exports = nextConfig
